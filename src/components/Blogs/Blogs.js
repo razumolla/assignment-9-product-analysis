07fbf4129f84c8478619a4d96a227a2fca8d3a3d
razumolla <rajuahmed9344@gmail.com>
2022-04-05 22:35:18 +0600

    ques and about section update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h3>blogs here</h3>
-        </div>
+        <section class='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+            <div class='grid grid-cols-1 gap-24 md:grid-cols-2'>
+                <div>
+                    <h1 class='mb-6 text-2xl font-light text-gray-900 md:text-3xl'>
+                        Basic Questions
+                    </h1>
+                    <p class='mt-10 mb-3 font-semibold text-gray-900'>
+                        What is Context API ?
+                    </p>
+                    <p class='text-gray-600'>
+                        The Context API can be used to share data with multiple components, and create global variables as, an alternative to passing props manually in a component tree, without having to pass data through props manually.
+                    </p>
+                    <p class='mt-10 mb-3 font-semibold text-gray-900'>
+                        What is Semantic tag ?
+                    </p>
+                    <p class='text-gray-600'>
+                        HTML5 semantic tags define the purpose of the element. By using semantic markup, you help the browser understand the meaning of the content instead of just displaying it.HTML5 semantic elements also help search engines to read the page and find the required information faster. Exam: header ,div,main,article,footer etc.
+                    </p>
+
+                </div>
+
+                <div>
+
+                    <p class='mt-20 mb-3 font-semibold text-gray-900'>
+                        Difference between Inline and Block element?
+                    </p>
+                    <p class='text-gray-600'>
+                        Inline elements don't start from a new line and don't have top and bottom margins as block elements have. Examples of Inline elements: img, a, input, label etc Block Element:Block Element always start in a new line and have top and bottom margins. It does not contain any other elements next to it. Examples of Block elements: div, h1-h6, p, ul, li etc
+                    </p>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,11 +12,12 @@ const Home = () => {
             <div className='home-container'>
                 <div className='pt-20'>
                     <h1 className='text-6xl font-bold text-pink-500  '>TECH BOSS  <small className='text-xl'>Mac Studio</small> </h1>
+                    <h2 className='text-3xl font-bold pt-8 text-blue-700 '>Latest MacBook Pro </h2>
                     <p className='mt-6 text-xl pr-10 pb-6'>
                         They make its laptops both more powerful and more power-efficient, meaning greatly improved performance and battery life. Plus, you get other perks, like the ability to run mobile apps originally made for iOS.
                     </p>
                     <Link to="/new-laptop">
-                        <button className='text-2xl bg-gray-200 text-blue-800 p-1 rounded'>
+                        <button className='text-2xl bg-blue-500 text-black-800 p-1 rounded'>
                             Buy Now</button>
                     </Link>
                 </div>
