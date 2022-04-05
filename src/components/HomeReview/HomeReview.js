33068610314cd21cbec8f8b89b7a2af4ceeeb39e
razumolla <rajuahmed9344@gmail.com>
2022-04-05 16:31:11 +0600

    customer home page rating icon update

--- src/components/HomeReview/HomeReview.js
+++ src/components/HomeReview/HomeReview.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './HomeReview.css';
+import { FaStarHalfAlt } from 'react-icons/fa';
+import { AiTwotoneStar, } from 'react-icons/ai';
 
 const HomeReview = (props) => {
     const { name, pic, feedback, ratting } = props.review;
@@ -9,7 +11,14 @@ const HomeReview = (props) => {
                 <img className='rounded-full' src={pic} alt="" />
             </div>
             <p className='grid justify-center text-3xl'>{name}</p>
-            <p className='grid justify-center text-2xl'> Ratting:{ratting}</p>
+            <p className='flex justify-center'>
+                <AiTwotoneStar className='text-yellow-500 text-2xl' />
+                <AiTwotoneStar className='text-yellow-500 text-2xl' />
+                <AiTwotoneStar className='text-yellow-500 text-2xl' />
+                <AiTwotoneStar className='text-yellow-500 text-2xl' />
+                <FaStarHalfAlt className='text-yellow-500 text-2xl' />
+            </p>
+            <p className='grid justify-center text-2xl'>{ratting}</p>
             <p>{feedback}</p>
         </div>
     );
